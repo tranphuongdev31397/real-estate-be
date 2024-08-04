0cf735d92a52995b26751fa18c268217d92ef322
Phuong Tran <phuong.tran@techvsi.com>
2024-08-04 15:23:55 +0700

    update: remove log and write document

--- src/utils/queryHandler.js
+++ src/utils/queryHandler.js
@@ -2,6 +2,16 @@ const { isEmpty, isObject } = require("lodash");
 const { Op } = require("sequelize");
 const { BadRequestError } = require("../core/error.response");
 
+/**
+ * The `searchHandler` function processes a search query to generate search conditions based on
+ * specified search fields and keywords.
+ * @param searchQuery - The `searchHandler` function takes in a `searchQuery` object as a parameter.
+ * The `searchQuery` object should have the following structure:
+ * @returns An object is being returned with a key of [Op.or] and a value of searchCondition, which is
+ * an object containing search conditions for each search field specified in the searchBy array. Each
+ * search condition uses the Sequelize operator Op.iLike to perform a case-insensitive search for the
+ * trimmed keyword within the specified search field.
+ */
 const searchHandler = (searchQuery) => {
   if (!searchQuery || isEmpty(searchQuery)) return {};
 
@@ -27,15 +37,24 @@ const searchHandler = (searchQuery) => {
   return { [Op.or]: searchCondition };
 };
 
+/**
+ * The function `searchByDefault` takes in a search term and search fields, and returns a search object
+ * with keyword and searchBy fields based on the input.
+ * @param search - The `search` parameter is used to specify the search query that the user wants to
+ * perform. It can be a string containing the keyword to search for or an object with the properties
+ * `keyword` and `searchBy`.
+ * @param searchByFields - `searchByFields` is an array that contains the fields that the user wants to
+ * search by.
+ * @returns The function `searchByDefault` will return an object with `keyword` and `searchBy`
+ * properties if the conditions in the code are met. If the conditions are not met, it will throw a
+ * `BadRequestError` with the message "Search Function is invalid".
+ */
 const searchByDefault = (search, searchByFields) => {
-  console.log("vo", search);
-
   if (!search) {
     return;
   }
   if (isObject(search) && !!search?.keyword && search?.searchBy?.length > 0) {
     // Case user defined searchBy
-    console.log("first");
 
     return search;
   }
