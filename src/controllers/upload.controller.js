35249eee4f0120b3a3b98972d9b54c1df49464da
Phuong Tran <phuong.tran@techvsi.com>
2024-08-31 10:55:09 +0700

    update: upload multiple

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,6 +1,7 @@
 const { BadRequestError } = require("../core/error.response");
 const { SuccessResponse } = require("../core/success.response");
 const UploadService = require("../services/upload.service");
+const { getInitData } = require("../utils");
 
 const uploadController = async (req, res, next) => {
   const { file, type } = req;
@@ -9,10 +10,37 @@ const uploadController = async (req, res, next) => {
   }
   const response = await UploadService.uploadSingle({ file });
   return new SuccessResponse({
-    metadata: response,
+    metadata: getInitData({
+      object: response,
+      fields: ["public_id", "mimetype", "secure_url"],
+    }),
+  }).send(res);
+};
+
+const uploadMultiController = async (req, res, next) => {
+  const { files, type } = req;
+
+  console.log(files);
+  if (!files || files.length === 0) {
+    throw new BadRequestError("No files uploaded.");
+  }
+
+  const responses = await Promise.all(
+    files.map(async (file) => {
+      const responseFile = await UploadService.uploadSingle({ file });
+      return getInitData({
+        object: responseFile,
+        fields: ["public_id", "mimetype", "secure_url"],
+      });
+    })
+  );
+
+  return new SuccessResponse({
+    metadata: responses,
   }).send(res);
 };
 
 module.exports = {
   uploadController,
+  uploadMultiController,
 };
--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -3,7 +3,7 @@ const { CLOUDINARY_FOLDERS } = require("../contants/cloudinary");
 const { BadRequestError } = require("../core/error.response");
 
 class UploadService {
-  static async uploadSingle({ file, options, resize }) {
+  static async uploadSingle({ file, options }) {
     const res = await cloudinary.uploader.upload(file.path, {
       folder: CLOUDINARY_FOLDERS.IMAGES,
       ...options,
@@ -12,7 +12,10 @@ class UploadService {
     if (!res) {
       throw new BadRequestError("Failed to upload file");
     }
-    return res;
+    return {
+      ...res,
+      mimetype: file.mimetype,
+    };
   }
 }
 
